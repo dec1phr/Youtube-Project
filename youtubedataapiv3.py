ddeb5a6d2873a5072fb01822e7345dca6fd7a6c9
Decyphr <bswsaritra004@gmail.com>
2026-03-16 03:22:24 +0600

    changed some logic

--- youtubedataapiv3.py
+++ youtubedataapiv3.py
@@ -3,12 +3,17 @@ from googleapiclient.discovery import build
 API_KEY = "AIzaSyAFdR_ceBa1gtOL5Pf7OT03Ic_LgHkjpPg"
 youtube = build("youtube", "v3", developerKey=API_KEY)
 
-def like_count(video_ids):
-    request = youtube.videos().list(
-        part="statistics",
-        id = video_ids[0]
-    )
-    response = request.execute()
-    likes = response["items"][0]["statistics"]["likeCount"]
-    return likes
+def like_count(top_results):
+    total_likes = {}
+    for i in top_results:
+        
+        request = youtube.videos().list(
+            part="statistics",
+            id = i
+        )
+        response = request.execute()
+        likes = int(response["items"][0]["statistics"].get("likeCount", 0))
+        total_likes.update({str(i): likes})
+        
+    return total_likes
     

d6b609b0be1403ef63f6dae424e0a14a94b68225
Decyphr <bswsaritra004@gmail.com>
2026-03-16 03:22:46 +0600

    changed some logic

--- view.py
+++ view.py
@@ -4,7 +4,7 @@ if __name__ == "view":
     
     def views(top_results):
         by_view =[]
-        sorted_views = list(islice(sorted(list(top_results.values()), reverse=True), 3))   
+        sorted_views = list(islice(sorted(list(top_results.values()), reverse=True), 10))   
         for key, value in top_results.items():
             if value in sorted_views:
                 by_view.append(key)

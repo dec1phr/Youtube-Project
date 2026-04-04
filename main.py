df9c347fde8e845f07578982b01be54b5d4a35eb
Decyphr <bswsaritra004@gmail.com>
2026-04-05 01:07:18 +0600

    Merged two files into one

--- main.py
+++ main.py
@@ -2,56 +2,59 @@ from googleapiclient.discovery import build
 import urllib.request
 import re
 import yt_dlp
-import view
-import youtubedataapiv3
+from itertools import islice
 
+API_KEY = "AIzaSyAFdR_ceBa1gtOL5Pf7OT03Ic_LgHkjpPg"
 
+def views(top_results):
+    by_view = {}
+    sorted_views = list(islice(sorted(list(top_results.values()), reverse=True), 3))
+    for key, value in top_results.items():
+        if value in sorted_views:
+            by_view.update({key: value})
+    return by_view
+
+def like_count(top_results):
+    youtube = build("youtube", "v3", developerKey=API_KEY)
+    total_likes = {}
+    for i in top_results:
+        request = youtube.videos().list(
+            part="statistics",
+            id=i
+        )
+        response = request.execute()
+        likes = int(response["items"][0]["statistics"].get("likeCount", 0))
+        total_likes.update({str(i): likes})
+    return total_likes
 
 if __name__ == '__main__':
-    
     print("-----------Welcome to Yinder ^^----------")
     print("-----------------------------------------")
     print("Choose options:")
-    target = int(input("1 - search normal stuffs \n2 - looking for something to learn"))
+    target = int(input("1 - search normal stuffs \n2 - looking for something to learn\n"))
+
     if target == 1:
         duration = int(input("Minimum Duration?: "))
-        #still haven't decide what to do next:(
-
-
-    
+        # still haven't decided what to do next :(
 
     usr = str(input("Your input goes here: "))
-    html = urllib.request.urlopen("https://www.youtube.com/results?search_query="+usr)
-
+    html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + usr)
     video_ids = list(re.findall(r"watch\?v=(\S{11})", html.read().decode()))
 
     ydl_opts = {}
-    top_results ={}
+    top_results = {}
 
-    for urls in video_ids[:20]: #the url contains individual urls #the video_ids contains all the 11 part of the links and it's basically a list
-        #url = str("https://www.youtube.com/watch/?v="+video_ids[0])
+    for urls in video_ids[:20]:
         with yt_dlp.YoutubeDL(ydl_opts) as ytdl:
             try:
                 info = ytdl.extract_info(urls, download=False)
-                top_results.update({str(urls):int(info["view_count"])})
+                top_results.update({str(urls): int(info["view_count"])})
             except:
                 pass
-            
-            
-    by_view = view.views(top_results) #stores the top 10 video urls(11 letter part) which has been sorted by views
-    youtubedata = youtubedataapiv3.like_count(by_view)
-    video = []
 
-    # keys = list(by_view.keys())
-    # views = list(by_view.values())
-    # likes = list(youtubedata.values())
+    by_view = views(top_results)
+    youtubedata = like_count(by_view)
 
+   
     for urls in by_view:
-        print("https://www.youtube.com/watch?v="+urls)
-        
-    
-
-
-    
-        
-    print(video)
+        print("https://www.youtube.com/watch?v=" + urls)
